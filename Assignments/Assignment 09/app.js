7da27fd0b2c18b29f8e5f84bc6c0c3adec879f6c
eishwarsoni40 <shirazizaid123@gmail.com>
2026-04-20 00:08:36 +0500

    added event listners & applied some improvements

--- Assignments/Assignment 09/app.js
+++ Assignments/Assignment 09/app.js
@@ -9,31 +9,36 @@ getlocalStorageData(); // calling function to get existing data first
 //1. Todo Add function
 function addTodo(){
     // Empty input validation
-    if(todoInput.value.length < 1){
+    if(todoInput.value.trim().length < 1){
         alert("Empty input detected! Please try again.");
         return;
     }
     // Minimum length validation
-    if(todoInput.value.length < 2){
+    if(todoInput.value.trim().length < 2){
         alert("Input length should be atleast 2 characters.");
         return;
     }
     // Maximum length validation
-    if(todoInput.value.length > 150){
+    if(todoInput.value.trim().length > 150){
         alert("Todo is too long.\nInput length cannot be greater than 150 characters.");
         return;
     }
 
     for(var i = 0; i<todoDatabase.length; i++){
-        if(todoDatabase[i].text === todoInput.value){
+
+        if(
+            todoDatabase[i].text.trim().toLowerCase() ===
+            todoInput.value.trim().toLowerCase()
+          ) {
             alert("This todo is already exists");
             return;
         }
+        
     }
 
-    // todo object
+    // todo object definition
     var todoObj = {
-        text: todoInput.value,
+        text: todoInput.value.trim(),// here trim() fixes the one space problem
         id:  Math.floor(Math.random() * 900000) + 100000, // 6 digit random id
         createdAt: new Date(),
         isCompleted: false,
@@ -43,10 +48,11 @@ function addTodo(){
     // storing the data in the local Storage
     window.localStorage.setItem("todos", JSON.stringify(todoDatabase));
 
-    console.log(todoDatabase); // for checking
+    // console.log(todoDatabase); // for checking
     renderTodo();
     todoInput.value = ""; // todo input value reset to empty
 }
+
 
 //2. Todo Render function
 
@@ -94,7 +100,7 @@ function renderTodo(){
         }
     }
 
-    if(todoDatabase.length == 0){
+    if(todoDatabase.length === 0){
         todoListContainer.style.backgroundImage = "url('./assets/images/todoPic.jpg')";
     } else{
         todoListContainer.style.backgroundImage = "none";
@@ -106,6 +112,7 @@ function renderTodo(){
         clearAllDiv.style.display = "none";
     }
 }
+
 
 //3. Todo Edit function
 
@@ -126,6 +133,7 @@ function editTodo(id) {
     todoInput.focus(); // for focus the input field automatically when the edit button is clicked
 }
 
+
 //4. Todo Update function
 
 function updateTodo(){
@@ -133,17 +141,17 @@ function updateTodo(){
     var saveButton = document.getElementById('saveButton');
 
     // Empty input validation
-    if(todoInput.value.length < 1){
+    if(todoInput.value.trim().length < 1){
         alert("Empty input detected! Please try again.");
         return;
     }
     // Minimum length validation
-    if(todoInput.value.length < 2){
+    if(todoInput.value.trim().length < 2){
         alert("Input length should be atleast 2 characters.");
         return;
     }
     // Maximum length validation
-    if(todoInput.value.length > 150){
+    if(todoInput.value.trim().length > 150){
         alert("Todo is too long.\nInput length cannot be greater than 150 characters.");
         return;
     }
@@ -153,9 +161,13 @@ function updateTodo(){
         /* the alert only shows when the clicking object and current object text matches also their id not matches
         (means the object skip itself)
         */        
-        if(todoDatabase[i].text === todoInput.value && todoDatabase[i].id !== todoToBeUpdate.id){
-        alert("This todo is already exists");
-        return;
+        if(
+            todoDatabase[i].text.trim().toLowerCase() ===
+            todoInput.value.trim().toLowerCase() &&
+            todoDatabase[i].id !== todoToBeUpdate.id
+          ) {
+            alert("This todo is already exists");
+            return;
         }
 
     }    
@@ -165,19 +177,26 @@ function updateTodo(){
     addButton.style.display = "block";
     saveButton.style.display = "none";
 
-    window.localStorage.setItem("todos", JSON.stringify(todoDatabase));// update the localStorage todos from todos array
+    // update the localStorage todos from todos array
+    window.localStorage.setItem("todos", JSON.stringify(todoDatabase));
+
+    // resetting variables so updateTodo() forgets the current todo 
+    indexToBeUpdate = null;
+    todoToBeUpdate = null;
     renderTodo();
 }
+
 
 //5. Todo Delete function
 
 function deleteTodo(id){
-   console.table(todoDatabase);
+//    console.table(todoDatabase);
    for (var i = 0; i < todoDatabase.length; i++) {
        if (todoDatabase[i].id === id) {
            todoDatabase.splice(i,1);
 
-           window.localStorage.setItem("todos", JSON.stringify(todoDatabase));// update the localStorage todos from todos array
+           // update the localStorage todos from todos array
+           window.localStorage.setItem("todos", JSON.stringify(todoDatabase));
 
            todoInput.value = "";
            if(saveButton.style.display === "block"){
@@ -189,7 +208,8 @@ function deleteTodo(id){
    }
 }
 
-//6.  function to get data from local storage
+
+//6.  Function to retrieve data from local storage (if exists)
 
 function getlocalStorageData(){
     
@@ -203,13 +223,14 @@ function getlocalStorageData(){
 
 }
 
+
 //7. Done todo function
 
 function doneTodo(id){
     
     for(var i = 0; i < todoDatabase.length; i++){
         
-        if(todoDatabase[i].id == id){
+        if(todoDatabase[i].id === id){
             todoDatabase[i].isCompleted = true;
             break;
         }
@@ -219,13 +240,18 @@ function doneTodo(id){
     renderTodo();
 }
 
+
 //8. Todo Delete all function
 
 function deleteAllTodo(){
+
     var saveButton = document.getElementById('saveButton');
-    window.localStorage.removeItem("todos"); // removes all todos from local storage
+
+    // removes all todos from local storage
+    window.localStorage.removeItem("todos"); 
     todoDatabase = []; // reseting the todo array
     todoInput.value = "";
+
     if(saveButton.style.display === "block"){
         saveButton.style.display = "none";
         addButton.style.display = "block";
@@ -233,3 +259,51 @@ function deleteAllTodo(){
     renderTodo();
 }
 
+
+/* *********************** EVENT LISTENERS CODE *********************** */
+
+// Triggers Add or Update logic when the enter key is pressed.
+todoInput.addEventListener("keydown", function(event){
+
+    var addButton = document.getElementById('addButton');
+    var saveButton = document.getElementById('saveButton');
+
+    if (event.key === "Enter") {
+        
+        /*This tells the browser: Stop your default action (like refreshing the page or submitting a form) and only do what I tell you to do next.*/
+        event.preventDefault();
+        
+        /*
+         Initial State: In HTML/CSS, the button is visible, but element.style.display only reads "inline styles".JavaScript sees it as an empty string. so "" !== "block". That's why i wrote condition like this.
+         */
+        if (
+            saveButton.style.display === "none" ||
+            saveButton.style.display === ""
+           ) {
+            addTodo();
+        }
+        else{
+            updateTodo();
+        }
+    }
+
+});
+
+// Replacing Save Button by Add Button when input length = 0
+todoInput.addEventListener("input", function() {
+
+    var addButton = document.getElementById('addButton');
+    var saveButton = document.getElementById('saveButton');
+
+    if (todoInput.value.trim().length === 0) {
+        if (saveButton.style.display === "block")
+        {
+            saveButton.style.display = "none";
+            addButton.style.display = "block";
+
+            // resetting variables so updateTodo() forgets the current todo
+            indexToBeUpdate = null;
+            todoToBeUpdate = null;
+        }
+    }
+});
