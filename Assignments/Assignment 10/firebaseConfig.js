9e96119ef0ea27a3a20ec30cb8b2cfae3a2bd416
eishwarsoni40 <shirazizaid123@gmail.com>
2026-06-06 07:23:31 +0500

    Registeration Form added

--- Assignments/Assignment 10/firebaseConfig.js
+++ Assignments/Assignment 10/firebaseConfig.js
@@ -1,7 +1,10 @@
 // Import the functions I need from the SDKs I need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
 
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
 
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-analytics.js";
 
@@ -21,7 +24,13 @@ import {
 // My web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-//
+  apiKey: "AIzaSyDePCmGlIIO44Ra9U3JynL9QYcVMd5y4Ds",
+  authDomain: "learning-db-61519.firebaseapp.com",
+  projectId: "learning-db-61519",
+  storageBucket: "learning-db-61519.firebasestorage.app",
+  messagingSenderId: "458264409328",
+  appId: "1:458264409328:web:120fee15577ecf1fbabe35",
+  measurementId: "G-KNQVMPR037",
 };
 
 // Initialize Firebase
@@ -34,4 +43,14 @@ const db = getFirestore(app);
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
 
-export { deleteDoc, doc, setDoc, addDoc, collection, db, getDocs, auth, createUserWithEmailAndPassword };
+export {
+  deleteDoc,
+  doc,
+  setDoc,
+  addDoc,
+  collection,
+  db,
+  getDocs,
+  auth,
+  createUserWithEmailAndPassword,
+};
